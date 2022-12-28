c609d6ce91a592340a21e18fd6dc982d0e6279cf
paulazoo <paulazhu@college.harvard.edu>
2022-12-28 13:28:04 -0500

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from './Box'
+import React, { useRef, useState, Suspense } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+       camera={{ position: [2, 0, 12.25], fov: 15 }}
+       style={{
+          backgroundColor: '#111a21',
+          width: '100vw',
+          height: '100vh',
+       }}
+    >
+      <ambientLight intensity={1.25} />
+      <ambientLight intensity={0.1} />
+      <directionalLight intensity={0.4} />
+      <pointLight position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Suspense>
+        <OrbitControls />
+    </Canvas>
   );
 }
 
-export default App;
+export default App
